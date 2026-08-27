2da6c54cb0ea58f564124f673ef66fb608a80d31
kvng-shaka <funsho.adeosun01@gmail.com>
2026-08-27 16:11:16 +0100

    Feat: user reg, login and me all done

--- users/models.py
+++ users/models.py
@@ -5,3 +5,23 @@ from django.db import models
 
 
 class User(AbstractUser):
+    class Role(models.TextChoices):
+        CUSTOMER = "customer", "Customer"
+        STAFF = "staff", "Staff"
+
+    role = models.CharField(max_length=20, choices=Role.choices, default=Role.CUSTOMER,)
+    phone_number = models.CharField(max_length=20,unique=True,)
+
+    def __str__(self):
+        return self.username
+
+
+class CustomerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile",)
+    date_of_birth = models.DateField(null=True, blank=True,)
+    address = models.TextField(blank=True,)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+
---
+++ users/serializers.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = [
+            "username", "email", "phone_number", "password",
+        ]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            phone_number=validated_data["phone_number"],
+            password=validated_data["password"],
+        )
+
+        return user
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id", "username", "email", "phone_number", "role",
+        ]
+
+        read_only_fields = [
+            "id", "role",
+        ]
+
